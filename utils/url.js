421b1593bf1946d3639aa0b1fc6b1124cf53d593
duzit <duwei_oneday@163.com>
2021-09-18 20:25:14 +0800

    update tools

--- utils/url.js
+++ utils/url.js
@@ -19,3 +19,22 @@
   }
   return value;
 };
+
+// url参数转对象
+export const parseQueryString = function(url) { //url参数转对象
+  url = !url ? window.location.href : url;
+  if (url.indexOf('?') === -1) {
+    return {};
+  }
+  let search = url[0] === '?' ? url.substr(1) : url.substring(url.lastIndexOf('?') + 1);
+  if (search === '') {
+    return {};
+  }
+  search = search.split('&');
+  let query = {};
+  for (let i = 0; i < search.length; i++) {
+    let pair = search[i].split('=');
+    query[decodeURIComponent(pair[0])] = decodeURIComponent(pair[1] || '');
+  }
+  return query;
+}
